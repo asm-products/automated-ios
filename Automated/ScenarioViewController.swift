98d81311594a98b48bb23588fa2b3abedaf7cf12
Nick Babenko <nickbabenko@me.com>
2014-11-22 21:36:04 +0000

    Created scenario view controller

---
+++ Automated/ScenarioViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ScenarioViewController.swift
+//  Automated
+//
+//  Created by Nick Babenko on 22/11/2014.
+//  Copyright (c) 2014 Automated. All rights reserved.
+//
+//  This view will show the user current available scenarios, which they previously configured.
+//  The scenario's loaded can be based on a number of things such as;
+//  current location, time of day and maybe even a custom picker, to select a custom set of scenario's.
+//
+
+import UIKit
+
+class ScenarioViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
